fb88d63d3e78bade3ac864eb92e4a8b818a5f379
OrranaLhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2019-06-16 09:35:20 -0300

    Update TestYen.java

--- matsim-example-project/src/main/java/org/matsim/project/router/TestYen.java
+++ matsim-example-project/src/main/java/org/matsim/project/router/TestYen.java
@@ -11,25 +11,8 @@ import org.matsim.core.scenario.ScenarioUtils;
 
 /**
  * Test of Yen's algorithm for computing the K shortest loopless paths between two nodes in a graph.
- *
- * Copyright (C) 2015  Brandon Smock (dr.brandon.smock@gmail.com, GitHub: bsmock)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Created by Brandon Smock on September 23, 2015.
- * Last updated by Brandon Smock on December 24, 2015.
  */
+
 public class TestYen {
 
     public static void main(String args[]) {
