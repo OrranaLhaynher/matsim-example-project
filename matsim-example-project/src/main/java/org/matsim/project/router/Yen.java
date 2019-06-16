a637870a8bfd4d28c6242339127b2fb6a80f8bd9
OrranaLhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2019-06-16 09:34:39 -0300

    Update Yen.java

--- matsim-example-project/src/main/java/org/matsim/project/router/Yen.java
+++ matsim-example-project/src/main/java/org/matsim/project/router/Yen.java
@@ -13,30 +13,12 @@ import org.matsim.core.router.util.LeastCostPathCalculator;
 import org.matsim.core.router.util.TravelDisutility;
 import org.matsim.core.router.util.TravelTime;
 import org.matsim.vehicles.Vehicle;
-
 import com.vividsolutions.jts.geomgraph.Edge;
 
 /**
  * Yen's algorithm for computing the K shortest loopless paths between two nodes in a graph.
- *
- * Copyright (C) 2015  Brandon Smock (dr.brandon.smock@gmail.com, GitHub: bsmock)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Created by Brandon Smock on September 23, 2015.
- * Last updated by Brandon Smock on December 24, 2015.
  */
+
 public final class Yen implements LeastCostPathCalculator {
 
     	private final Network network;
